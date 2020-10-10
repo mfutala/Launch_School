c6c834e9a3a4161eaf50fedb947b5c4cffdf0a0a
Mike Futala <mfutala@hotmail.com>
2020-10-10 13:39:38 -0700

    10/10/20 from Starbucks

---
+++ books/introToProgJS/test.js
@@ -0,0 +1,22 @@
+/* let arr = [1,2,3];
+
+console.log(arr.length);
+console.log(arr);
+arr.length = 2;
+console.log(arr.length);
+console.log(arr);
+arr.length = 3;
+console.log(arr.length);
+console.log(arr);
+console.log(arr[2]); */
+
+let arr = [1,3,6,11,4,2,4,9,17,16,0];
+
+arr.slice().sort().forEach(element => { 
+    if (element % 2 === 0) {
+        console.log(element);
+    }
+    
+});
+
+console.log(arr);
